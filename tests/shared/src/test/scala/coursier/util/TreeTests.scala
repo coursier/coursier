89fa617a20c0082f5863378b152e8d6507c80e82
wisechengyi <wisechengyi@gmail.com>
2017-10-17 16:08:11 -0700

    more tests

--- tests/shared/src/test/scala/coursier/util/TreeTests.scala
+++ tests/shared/src/test/scala/coursier/util/TreeTests.scala
@@ -3,6 +3,7 @@ package coursier.util
 import utest._
 
 object TreeTests extends TestSuite {
+
   case class Node(label: String, children: Node*)
 
   val roots = Array(
@@ -14,15 +15,36 @@ object TreeTests extends TestSuite {
       Node("c4"))
   )
 
+  val moreNestedRoots = Array(
+    Node("p1",
+      Node("c1",
+        Node("p2"))),
+    Node("p2",
+      Node("c1"))
+  )
+
+
   val tests = TestSuite {
     'apply {
       val str = Tree[Node](roots)(_.children, _.label)
-      assert(str == """├─ p1
+      assert(str ==
+        """├─ p1
         #│  ├─ c1
         #│  └─ c2
         #└─ p2
         #   ├─ c3
         #   └─ c4""".stripMargin('#'))
+    }
+
+    'apply {
+      val str = Tree[Node](moreNestedRoots)(_.children, _.label)
+      println(str)
+      assert(str ==
+        """├─ p1
+        #│  └─ c1
+        #│     └─ p2
+        #└─ p2
+        #   └─ c1""".stripMargin('#'))
     }
   }
 }
