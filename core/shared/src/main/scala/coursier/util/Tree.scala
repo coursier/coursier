18c4978e02d2b0fddaf3db24fe5bdc696461bfad
wisechengyi <wisechengyi@gmail.com>
2017-10-17 13:15:16 -0700

    simplify

--- core/shared/src/main/scala/coursier/util/Tree.scala
+++ core/shared/src/main/scala/coursier/util/Tree.scala
@@ -1,73 +1,29 @@
 package coursier.util
 
-import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 object Tree {
 
   def apply[T](roots: IndexedSeq[T])(children: T => Seq[T], show: T => String): String = {
 
-    val buffer = new StringBuilder
-    val printLine: (String) => Unit = { line =>
-      buffer.append(line).append('\n')
-    }
 
-    def last[E, O](seq: Seq[E])(f: E => O) =
-      seq.takeRight(1).map(f)
-    def init[E, O](seq: Seq[E])(f: E => O) =
-      seq.dropRight(1).map(f)
+    def recursivePrint(roots: Seq[T], ancestors: Set[T], prefix: String, acc: String => Unit): Unit = {
+      for (root <- roots) {
+        if (ancestors.contains(root)) {
+          return
+        }
+        val isLast = roots.indexOf(root) == roots.length - 1
+        val tee = if (isLast) "└─ " else "├─ "
+        acc(prefix + tee + show(root))
 
-    /*
-     * Add elements to the stack
-     * @param elems elements to add
-     * @param isLast a list that contains whether an element is the last in its siblings or not.
-     */
-    def childrenWithLast(elems: Seq[T],
-                         isLast: Seq[Boolean]): Seq[(T, Seq[Boolean])] = {
-
-      val isNotLast = isLast :+ false
-
-      init(elems)(_ -> isNotLast) ++
-        last(elems)(_ -> (isLast :+ true))
-    }
-
-    /**
-      * Has to end with a "─"
-      */
-    def showLine(isLast: Seq[Boolean]): String = {
-      val initPrefix = init(isLast) {
-        case true => "   "
-        case false => "│  "
-      }.mkString
-
-      val lastPrefix = last(isLast) {
-        case true => "└─ "
-        case false => "├─ "
-      }.mkString
-
-      initPrefix + lastPrefix
-    }
-
-    // Depth-first traverse
-    @tailrec
-    def helper(stack: Seq[(T, Seq[Boolean])], seen: Set[T]): Unit = {
-      stack match {
-        case (elem, isLast) +: next =>
-          printLine(showLine(isLast) + show(elem))
-
-          if (!seen(elem))
-            helper(childrenWithLast(children(elem), isLast) ++ next,
-                   seen + elem)
-          else
-            helper(next, seen)
-        case Seq() =>
+        val extraPrefix = if (isLast) "   " else "|  "
+        recursivePrint(children(root), ancestors + root, prefix + extraPrefix, acc)
       }
     }
 
-    helper(childrenWithLast(roots, Vector[Boolean]()), Set.empty)
-
-    buffer
-      .dropRight(1) // drop last appended '\n'
-      .toString
+    val b = new ArrayBuffer[String]
+    recursivePrint(roots, Set(), "", b += _)
+    b.mkString("\n")
   }
 
 }
