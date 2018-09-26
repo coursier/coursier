3b48fde5f82286565e38bfc0550bd6984f3b7893
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-09-26 18:54:43 +0200

    Process website md files with mdoc

--- scripts/generate-website.sh
+++ scripts/generate-website.sh
@@ -12,7 +12,22 @@ fi
 VERSION="$("$GREP" -oP '(?<=")[^"]*(?<!")' ../version.sbt)"
 echo "Current version is $VERSION"
 
+echo "Processing Markdown files"
+
+# first processing md files via https://github.com/olafurpg/mdoc
+# requires the cache modules and its dependencies to have been published locally
+# with
+#   sbt coreJVM/publishLocal cacheJVM/publishLocal
+../coursier launch \
+  com.geirsson:mdoc_2.12.6:0.4.5 \
+  "io.get-coursier:coursier-cache_2.12:$VERSION" \
+  -- \
+    --in ../doc/docs \
+    --out ../doc/processed-docs
+
 # website generation based on https://github.com/szeiger/ornate
+
+echo "Generating website"
 
 java -Dversion="$VERSION" -jar ../coursier launch com.novocode:ornate_2.11:0.5 -- ../doc/ornate.conf
 
@@ -20,3 +35,17 @@ DIR="$(cd ../doc/generated; pwd)"
 
 echo
 echo "Generated website available under $DIR"
+
+
+if echo "$OSTYPE" | grep -q darwin; then
+  OPEN_CMD="open"
+else
+  OPEN_CMD="xdg-open"
+fi
+
+cat << EOF
+Open the generated website with
+
+  $OPEN_CMD doc/generated/index.html
+
+EOF
