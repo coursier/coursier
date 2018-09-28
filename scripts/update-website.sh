22bbeeeb36087754701785ed7a50a60cd0f74856
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-09-28 16:22:48 +0200

    Validate website from PRs too

--- scripts/update-website.sh
+++ scripts/update-website.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 sbt scala212 coreJVM/publishLocal cacheJVM/publishLocal
 
-./generate-website.sh
+./scripts/generate-website.sh
 
-./push-website.sh
+if [ "${PUSH_WEBSITE:-""}" = 1 ]; then
+  ./scripts/push-website.sh
+fi
 
