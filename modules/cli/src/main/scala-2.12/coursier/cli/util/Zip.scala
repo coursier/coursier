655efbd09e9997e2de19417cf878cf8a587626e7
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-10-12 11:38:41 +0200

    Ensure commands that process zip files are fine with custom deflaters

--- modules/cli/src/main/scala-2.12/coursier/cli/util/Zip.scala
+++ modules/cli/src/main/scala-2.12/coursier/cli/util/Zip.scala
@@ -19,6 +19,13 @@ object Zip {
 
         update()
 
+        // ZipInputStream seems not to be fine with custom deflaters, like some recent versions of proguard use.
+        // This makes ZipOutputStream not handle some of these entries fine without this.
+        // See https://github.com/spring-projects/spring-boot/issues/13720#issuecomment-403428384.
+        // Same workaround as https://github.com/spring-projects/spring-boot/issues/13720
+        // (https://github.com/spring-projects/spring-boot/commit/a50646b7cc3ad941e748dfb450077e3a73706205#diff-2ff64cd06c0b25857e3e0dfdb6733174R144)
+        ent.setCompressedSize(-1L)
+
         (ent, data)
       }
     }
