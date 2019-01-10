90f04a9cc340d9f82f9489601a921680e7b1203c
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-10 11:14:01 +0100

    Tweak update website stage

--- scripts/update-website.sh
+++ scripts/update-website.sh
@@ -7,6 +7,7 @@ if [ "$TRAVIS_BRANCH" != "" ]; then
   source scripts/setup-build-tools.sh
   npm install
   npm install bower
+  export PATH="$PATH:$(pwd)/node_modules/bower/bin"
 fi
 
 mill -i all doc.publishLocal doc.docusaurus.yarnRunBuild doc.docusaurus.postProcess
