ad90041d996f020431fb2f6245c57f9ab0bd3bd5
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-10 16:43:14 +0100

    Switch to latest sbt-extras

--- scripts/setup-build-tools.sh
+++ scripts/setup-build-tools.sh
@@ -4,7 +4,7 @@ set -e
 export PATH="$(pwd)/bin:$PATH"
 
 mkdir -p bin
-curl -L -o bin/sbt https://raw.githubusercontent.com/paulp/sbt-extras/33b1a535656222810572d36d10afc5711515958e/sbt
+curl -L -o bin/sbt https://raw.githubusercontent.com/paulp/sbt-extras/62e7b13b3886cc36c2f3a5a4e9dd161b94657d09/sbt
 chmod +x bin/sbt
 
 curl -Lo bin/mill https://github.com/lihaoyi/mill/releases/download/0.3.5/0.3.5
