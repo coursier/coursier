1457b21214d1d4d8e03fa56ae8f8764f1bcaeda8
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-11 15:05:13 +0100

    Upload launcher to gh-pages upon release

---
+++ scripts/upload-launcher-gh-pages.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+if [[ ${TRAVIS_TAG} != v* ]]; then
+  echo "Not on a git tag"
+  exit 1
+fi
+
+export VERSION="$(echo "$TRAVIS_TAG" | sed 's@^v@@')"
+
+mkdir -p target
+cd target
+
+if [ -d gh-pages ]; then
+  echo "Removing former gh-pages clone"
+  rm -rf gh-pages
+fi
+
+echo "Cloning"
+git clone "https://${GH_TOKEN}@github.com/coursier/coursier.git" -q -b gh-pages gh-pages
+cd gh-pages
+
+git config user.name "Travis-CI"
+git config user.email "invalid@travis-ci.com"
+
+curl -Lo coursier "https://github.com/coursier/coursier/releases/download/$TRAVIS_TAG/coursier"
+curl -Lo coursier.bat "https://github.com/coursier/coursier/releases/download/$TRAVIS_TAG/coursier.bat"
+
+git add -- coursier coursier.bat
+
+MSG="Add $VERSION launcher"
+
+# probably not fine with i18n
+if git status | grep "nothing to commit" >/dev/null 2>&1; then
+  echo "Nothing changed"
+else
+  git commit -m "$MSG"
+
+  echo "Pushing changes"
+  git push origin gh-pages
+fi
