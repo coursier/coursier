90c956531db4f74814c887389b067ee1536a1a9e
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-14 14:22:07 +0100

    Move some coursier-specific logic to .travis.yml

--- scripts/update-website.sh
+++ scripts/update-website.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-npm install
-npm install bower
-git checkout -- package.json
-export PATH="$PATH:$(pwd)/node_modules/bower/bin"
-
 mkdir -p target
 git clone https://github.com/coursier/versioned-docs.git target/versioned-docs
 cd target/versioned-docs
@@ -15,8 +10,3 @@ cd -
 git status
 
 mill -i all doc.publishLocal doc.docusaurus.yarnRunBuild doc.docusaurus.postProcess
-
-if [ "${PUSH_WEBSITE:-""}" = 1 ]; then
-  ./scripts/push-website.sh
-fi
-
