05190b64729c5ac427da956c293ab03745d51dc3
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-28 10:42:18 +0100

    Embed completions as resources
    Print them via e.g.
    ```bash
    $ coursier --completions zsh
    ```

--- modules/cli/src/main/scala-2.12/coursier/cli/Coursier.scala
+++ modules/cli/src/main/scala-2.12/coursier/cli/Coursier.scala
@@ -1,15 +1,46 @@
 package coursier
 package cli
 
-import caseapp.core.app.CommandAppA
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+
+import caseapp.core.help.Help
+import caseapp.core.parser.Parser
+import coursier.bootstrap.util.FileUtil
+import coursier.cli.options.LauncherOptions
 import coursier.cli.resolve.Resolve
 import shapeless._
 
-object Coursier extends CommandAppA(CoursierCommand.parser, CoursierCommand.help) {
+object Coursier extends CommandAppPreA(Parser[LauncherOptions], Help[LauncherOptions], CoursierCommand.parser, CoursierCommand.help) {
 
   override val appName = "Coursier"
   override val progName = "coursier"
   override val appVersion = coursier.util.Properties.version
+
+  private def zshCompletions(): String = {
+    var is: InputStream = null
+    val b = try {
+      is = Thread.currentThread()
+        .getContextClassLoader
+        .getResource("completions/zsh")
+        .openStream()
+      FileUtil.readFully(is)
+    } finally {
+      if (is != null)
+        is.close()
+    }
+    new String(b, StandardCharsets.UTF_8)
+  }
+
+  def beforeCommand(options: LauncherOptions, remainingArgs: Seq[String]): Unit =
+    options.completions.foreach {
+      case "zsh" =>
+        System.out.print(zshCompletions())
+        sys.exit(0)
+      case other =>
+        System.err.println(s"Unrecognized or unsupported shell: $other")
+        sys.exit(1)
+    }
 
   def runA =
     args => {
