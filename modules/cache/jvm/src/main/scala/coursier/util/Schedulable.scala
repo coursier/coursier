039f31c929b3bf266592bb9d1b1fedd991a89853
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-29 14:32:29 +0100

    Tweaking / add comment

--- modules/cache/jvm/src/main/scala/coursier/util/Schedulable.scala
+++ modules/cache/jvm/src/main/scala/coursier/util/Schedulable.scala
@@ -2,8 +2,6 @@ package coursier.util
 
 import java.util.concurrent.{ExecutorService, Executors, ThreadFactory}
 
-import scala.util.control.NonFatal
-
 trait Schedulable[F[_]] extends Gather[F] {
   def delay[A](a: => A): F[A]
   def handle[A](a: F[A])(f: PartialFunction[Throwable, A]): F[A]
@@ -12,7 +10,10 @@ trait Schedulable[F[_]] extends Gather[F] {
 
   def attempt[A](f: F[A]): F[Either[Throwable, A]] =
     handle(map(f)(Right(_): Either[Throwable, A])) {
-      case NonFatal(t) => Left(t)
+      // in the case of Task, fatal errors are trapped anyway here, as it is backed by scala.concurrent.Future,
+      // causing some Task to never complete…
+      // (https://stackoverflow.com/questions/32641464/exception-causes-future-to-never-complete)
+      case t: Throwable => Left(t)
     }
 }
 
