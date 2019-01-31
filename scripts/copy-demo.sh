158672980353f72dfa699ccae23271499f0af609
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-31 12:19:30 +0100

    Fix script

--- scripts/copy-demo.sh
+++ scripts/copy-demo.sh
@@ -3,8 +3,8 @@ set -euv
 
 sbt web/fastOptJS::webpack
 
-mkdir -p "$DEST"
 DEST="$WEBSITE_DIR/build/coursier/demo"
+mkdir -p "$DEST"
 
 cp modules/web/target/scala-2.12/scalajs-bundler/main/web-fastopt-bundle.js "$DEST/"
 cp modules/web/target/scala-2.12/classes/bundle.js "$DEST/"
