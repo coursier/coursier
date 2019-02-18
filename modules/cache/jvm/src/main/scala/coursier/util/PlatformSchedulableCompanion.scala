bfe1420ea2251c886853d1940c5297489c6759a0
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-02-18 17:30:23 +0100

    Better default thread pool

--- modules/cache/jvm/src/main/scala/coursier/util/PlatformSchedulableCompanion.scala
+++ modules/cache/jvm/src/main/scala/coursier/util/PlatformSchedulableCompanion.scala
@@ -1,23 +1,12 @@
 package coursier.util
-import java.util.concurrent.{ExecutorService, Executors, ThreadFactory}
+
+import java.util.concurrent.ExecutorService
+
+import coursier.cache.internal.ThreadUtil
 
 abstract class PlatformSchedulableCompanion {
 
-  lazy val defaultThreadPool =
-    fixedThreadPool(4 max Runtime.getRuntime.availableProcessors())
-
-  def fixedThreadPool(size: Int): ExecutorService =
-    Executors.newFixedThreadPool(
-      size,
-      // from scalaz.concurrent.Strategy.DefaultDaemonThreadFactory
-      new ThreadFactory {
-        val defaultThreadFactory = Executors.defaultThreadFactory()
-        def newThread(r: Runnable) = {
-          val t = defaultThreadFactory.newThread(r)
-          t.setDaemon(true)
-          t
-        }
-      }
-    )
+  private[coursier] def fixedThreadPool(size: Int): ExecutorService =
+    ThreadUtil.fixedThreadPool(size)
 
 }
