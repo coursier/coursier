625921afd4e5c56b25aed5f82714957c3d85daa7
Scala steward <me@scala-steward.org>
2019-02-19 20:47:03 +0100

    Update docker-client to 8.15.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -17,7 +17,7 @@ object Deps {
   def argonautShapeless = "com.github.alexarchambault" %% "argonaut-shapeless_6.2" % "1.2.0-M8"
   def scalatest = "org.scalatest" %% "scalatest" % "3.0.5"
   def junit = "junit" % "junit" % "4.12"
-  def dockerClient = "com.spotify" % "docker-client" % "8.15.0"
+  def dockerClient = "com.spotify" % "docker-client" % "8.15.1"
 
   def sbtPgp = Def.setting {
     val sbtv = CrossVersion.binarySbtVersion(sbtVersion.in(pluginCrossBuild).value)
