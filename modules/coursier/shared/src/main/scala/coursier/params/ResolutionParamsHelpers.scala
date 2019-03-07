2a54c70c9ccf337bceac4d0eb645f0337801df47
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-03-07 15:51:34 +0100

    Add helper method

--- modules/coursier/shared/src/main/scala/coursier/params/ResolutionParamsHelpers.scala
+++ modules/coursier/shared/src/main/scala/coursier/params/ResolutionParamsHelpers.scala
@@ -6,9 +6,11 @@ import coursier.params.rule.{Rule, RuleResolution}
 abstract class ResolutionParamsHelpers {
   def forceScalaVersion: Option[Boolean]
   def scalaVersion: Option[String]
-  def withRules(rules: Seq[(Rule, RuleResolution)]): coursier.params.ResolutionParams
+  def withRules(rules: Seq[(Rule, RuleResolution)]): ResolutionParams
   def forceVersion: Map[Module, String]
   def withForceVersion(fv: Map[Module, String]): ResolutionParams
+  def profiles: Set[String]
+  def withProfiles(profiles: Set[String]): ResolutionParams
 
   def addForceVersion(fv: (Module, String)*): ResolutionParams =
     withForceVersion(forceVersion ++ fv)
@@ -21,6 +23,9 @@ abstract class ResolutionParamsHelpers {
     scalaVersion.getOrElse {
       coursier.internal.Defaults.scalaVersion
     }
+
+  def addProfile(profile: String*): ResolutionParams =
+    withProfiles(profiles ++ profile)
 
   final def addRule(rule: Rule, resolution: RuleResolution): coursier.params.ResolutionParams =
     withRules(Seq(rule -> resolution))
