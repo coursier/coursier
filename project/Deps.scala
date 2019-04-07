25be979b5ee7af0a5efea97fb26a82fbccdfd60d
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-04-07 23:46:10 +0200

    Update scalatest to 3.0.7 (#1116)

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,7 +15,7 @@ object Deps {
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M6"
   def okhttpUrlConnection = "com.squareup.okhttp" % "okhttp-urlconnection" % "2.7.5"
   def argonautShapeless = "com.github.alexarchambault" %% "argonaut-shapeless_6.2" % "1.2.0-M10"
-  def scalatest = "org.scalatest" %% "scalatest" % "3.0.6"
+  def scalatest = "org.scalatest" %% "scalatest" % "3.0.7"
   def junit = "junit" % "junit" % "4.12"
   def dockerClient = "com.spotify" % "docker-client" % "8.15.2"
 
