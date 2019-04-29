5b71c8e3cbc036f9918b87db7d0ea8fe35bcd07d
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2019-04-29 17:30:00 +0200

    Switch to scala-native 0.3.9 (#1140)

--- project/SharedVersions.scala
+++ project/SharedVersions.scala
@@ -5,7 +5,7 @@ object SharedVersions {
   def asm = "5.2"
   def fastParse = "2.1.2"
   def proguard = "6.0.3"
-  def scalaNative = "0.3.8"
+  def scalaNative = "0.3.9"
   def scalaz = "7.2.27"
 
 }
