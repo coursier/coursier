1e2199ce192e1a241f745d0f7d4d4f977b7b0a11
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-05-10 12:36:47 +0200

    Clean-up dependencies

--- project/SharedVersions.scala
+++ project/SharedVersions.scala
@@ -2,10 +2,8 @@
 object SharedVersions {
 
   def argonautShapeless = "1.2.0-M11"
-  def asm = "5.2"
   def fastParse = "2.1.2"
   def proguard = "6.0.3"
-  def scalaNative = "0.3.9"
   def scalaz = "7.2.27"
 
 }
