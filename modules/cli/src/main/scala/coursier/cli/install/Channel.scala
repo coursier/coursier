27c132a04cf6f8763db65e54529058515a32d1a3
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-06-06 18:06:18 +0200

    Rework channel URLs from github
    So that one can pass the json web page as a channel, rather than the raw
    link.

--- modules/cli/src/main/scala/coursier/cli/install/Channel.scala
+++ modules/cli/src/main/scala/coursier/cli/install/Channel.scala
@@ -1,5 +1,7 @@
 package coursier.cli.install
 
+import java.util.regex.Pattern.quote
+
 import coursier.core.Module
 
 abstract class Channel extends Product with Serializable {
@@ -21,7 +23,27 @@ object Channel {
   def module(module: Module): FromModule =
     FromModule(module)
 
-  def url(url: String): FromUrl =
-    FromUrl(url)
+  private lazy val ghUrlMatcher =
+    (quote("https://github.com/") + "([^/]*)/([^/]*)" + quote("/blob/") + "([^/]*)" + quote("/") + "(.*)").r.pattern
+
+  def url(url: String): FromUrl = {
+
+    val m = ghUrlMatcher.matcher(url)
+
+    val url0 =
+      if (m.matches()) {
+        val org = m.group(1)
+        val name = m.group(2)
+        val branch = m.group(3)
+        val path = m.group(4)
+        s"https://raw.githubusercontent.com/$org/$name/$branch/$path"
+      } else
+        url
+
+    // https://github.com/coursier/apps/blob/master/apps/resources/ammonite.json
+    // https://raw.githubusercontent.com/coursier/apps/master/apps/resources/ammonite.json
+
+    FromUrl(url0)
+  }
 
 }
