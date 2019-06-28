9e6c3bc6a592713e963f7d2435230b9ab29a5001
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-06-28 10:01:42 +0200

    Update scalaz-concurrent, scalaz-core to 7.2.28 (#1237)

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,7 +15,7 @@ object Deps {
     }
     def http4s = "0.18.17"
     def monadless = "0.0.13"
-    def scalaz = "7.2.27"
+    def scalaz = "7.2.28"
   }
 
   def argonautShapeless = "com.github.alexarchambault" %% "argonaut-shapeless_6.2" % versions.argonautShapeless
