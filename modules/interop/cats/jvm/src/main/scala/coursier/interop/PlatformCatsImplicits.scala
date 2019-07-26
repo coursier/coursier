b3696fa920d5d8fa1b4ae41be510082458421b40
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-07-26 14:28:34 +0200

    Make implicit Sync conversion more generic in cats-interop module

--- modules/interop/cats/jvm/src/main/scala/coursier/interop/PlatformCatsImplicits.scala
+++ modules/interop/cats/jvm/src/main/scala/coursier/interop/PlatformCatsImplicits.scala
@@ -2,42 +2,35 @@ package coursier.interop
 
 import java.util.concurrent.ExecutorService
 
-import _root_.cats.effect.IO
 import _root_.cats.instances.vector._
-import _root_.cats.syntax.apply._
+import _root_.cats.syntax.all._
 import coursier.util.Sync
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
 
 abstract class PlatformCatsImplicits {
 
-  // wish cs wasn't needed here (parSequence in gather needs it)
-  implicit def catsIOSync(implicit cs: _root_.cats.effect.ContextShift[IO]): Sync[IO] =
-    new Sync[IO] {
-      def point[A](a: A) =
-        IO.pure(a)
-      def delay[A](a: => A) =
-        IO(a)
-      override def fromAttempt[A](a: Either[Throwable, A]): IO[A] =
-        IO.fromEither(a)
-      def handle[A](a: IO[A])(f: PartialFunction[Throwable, A]) =
-        a.handleErrorWith { e =>
-          f.lift(e).fold[IO[A]](IO.raiseError(e))(IO.pure)
-        }
-      def schedule[A](pool: ExecutorService)(f: => A) = {
-
+  implicit def coursierSyncFromCats[F[_], F0[_]](implicit N: _root_.cats.effect.Sync[F], par: _root_.cats.Parallel[F, F0], cs: _root_.cats.effect.ContextShift[F]): Sync[F] =
+    new Sync[F] {
+      def point[A](a: A): F[A] =
+        a.pure[F]
+      def delay[A](a: => A): F[A] =
+        N.delay(a)
+      override def fromAttempt[A](a: Either[Throwable, A]): F[A] =
+        N.fromEither(a)
+      def handle[A](a: F[A])(f: PartialFunction[Throwable, A]): F[A] =
+        a.recover(f)
+      def schedule[A](pool: ExecutorService)(f: => A): F[A] = {
         val ec0 = pool match {
           case eces: ExecutionContextExecutorService => eces
           case _ => ExecutionContext.fromExecutorService(pool) // FIXME Is this instantiation costly? Cache it?
         }
-
-        IO.shift(ec0) *> IO(f)
+        cs.evalOn(ec0)(N.delay(f))
       }
 
-      def gather[A](elems: Seq[IO[A]]) =
-        _root_.cats.Parallel.parSequence(elems.toVector).map(_.toSeq)
-
-      def bind[A, B](elem: IO[A])(f: A => IO[B]) =
+      def gather[A](elems: Seq[F[A]]): F[Seq[A]] =
+        N.map(_root_.cats.Parallel.parSequence(elems.toVector))(_.toSeq)
+      def bind[A, B](elem: F[A])(f: A => F[B]): F[B] =
         elem.flatMap(f)
     }
 
