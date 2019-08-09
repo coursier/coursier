c0a93d4b303bd1c000ae0cb813edcc9f5b00590d
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2019-08-09 09:44:06 +0200

    Fail with appropriate error message if main class is not static (#1310)

--- modules/cli/src/main/scala/coursier/cli/launch/LaunchException.scala
+++ modules/cli/src/main/scala/coursier/cli/launch/LaunchException.scala
@@ -1,5 +1,7 @@
 package coursier.cli.launch
 
+import java.lang.reflect.Method
+
 sealed abstract class LaunchException(message: String, cause: Throwable = null) extends Exception(message, cause)
 
 object LaunchException {
@@ -7,6 +9,7 @@ object LaunchException {
   final class NoMainClassFound extends LaunchException("Cannot find default main class")
   final class MainClassNotFound(mainClass: String, cause: Throwable) extends LaunchException(s"Class $mainClass not found", cause)
   final class MainMethodNotFound(mainClass: Class[_], cause: Throwable) extends LaunchException(s"Main method not found in $mainClass", cause)
+  final class NonStaticMainMethod(mainClass: Class[_], method: Method) extends LaunchException(s"Main method in $mainClass is not static")
 
   // constraint to be lifted later
   final class ClasspathHierarchyUnsupportedWhenForking extends LaunchException("Cannot start an application with a hierarchical classpath by forking")
