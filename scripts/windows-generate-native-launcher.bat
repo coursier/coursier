88a6ca19a34f7755bfa6323c5e091bef706b5643
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-02-11 12:06:13 +0100

    Tweak windows-generate-native-launcher.bat script

--- scripts/windows-generate-native-launcher.bat
+++ scripts/windows-generate-native-launcher.bat
@@ -4,4 +4,6 @@
 @rem You also need to run a "sbt publishLocal", note the version it published,
 @rem and pass it to this script like --version 2.0.0-...-SNAPSHOT
 
+@call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd"
+
 @call modules\cli\target\pack\bin\coursier.bat launch --jvm graalvm:19.3 ammonite:2.0.4 -- launcher.sc generateNativeImage --output cs-dev %*
