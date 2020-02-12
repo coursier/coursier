52295d79d3bd54b415320ad2eb38fbf8cf501d97
Travis-CI <alexandre.archambault@gmail.com>
2020-02-12 02:15:56 +0100

    Remove unused command install-path

--- modules/cli/src/main/scala/coursier/cli/Coursier.scala
+++ modules/cli/src/main/scala/coursier/cli/Coursier.scala
@@ -10,7 +10,7 @@ import caseapp.core.parser.Parser
 import coursier.cli.bootstrap.Bootstrap
 import coursier.cli.complete.Complete
 import coursier.cli.fetch.Fetch
-import coursier.cli.install.{Install, InstallPath, Update}
+import coursier.cli.install.{Install, Update}
 import coursier.cli.jvm.{Java, JavaHome}
 import coursier.cli.launch.Launch
 import coursier.cli.publish.Publish
@@ -102,27 +102,25 @@ object Coursier extends CommandAppPreA(Parser[LauncherOptions], Help[LauncherOpt
         Fetch.run(fetchOptions, args)
       case Inr(Inr(Inr(Inl(installOptions)))) =>
         Install.run(installOptions, args)
-      case Inr(Inr(Inr(Inr(Inl(installPathOptions))))) =>
-        InstallPath.run(installPathOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inl(javaOptions)))))) =>
+      case Inr(Inr(Inr(Inr(Inl(javaOptions))))) =>
         Java.run(javaOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inl(javaHomeOptions))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inl(javaHomeOptions)))))) =>
         JavaHome.run(javaHomeOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(launchOptions)))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inl(launchOptions))))))) =>
         Launch.run(launchOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(publishOptions))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(publishOptions)))))))) =>
         Publish.run(publishOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(resolveOptions)))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(resolveOptions))))))))) =>
         Resolve.run(resolveOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(setupOptions))))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(setupOptions)))))))))) =>
         Setup.run(setupOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(sonatypeOptions)))))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(sonatypeOptions))))))))))) =>
         Sonatype.run(sonatypeOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(sparkSubmitOptions))))))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(sparkSubmitOptions)))))))))))) =>
         SparkSubmit.run(sparkSubmitOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(updateOptions)))))))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inl(updateOptions))))))))))))) =>
         Update.run(updateOptions, args)
-      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(cnil)))))))))))))) =>
+      case Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(Inr(cnil))))))))))))) =>
         cnil.impossible
     }
 
