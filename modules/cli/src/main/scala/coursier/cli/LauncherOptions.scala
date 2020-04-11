0be00e042318bd1eb81ab75212aa77d3ec553f24
Filipe Regadas <filiperegadas@gmail.com>
2020-04-11 09:28:23 -0400

    Add --version

--- modules/cli/src/main/scala/coursier/cli/LauncherOptions.scala
+++ modules/cli/src/main/scala/coursier/cli/LauncherOptions.scala
@@ -1,9 +1,11 @@
 package coursier.cli
 
 import caseapp.core.help.Help
-import caseapp.{Hidden, Parser}
+import caseapp.{Hidden, Parser, HelpMessage}
 
 final case class LauncherOptions(
+  @Hidden
+  version: Boolean = false,
   @Hidden
     completions: Option[String] = None,
 
