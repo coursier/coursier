50e2105eb987c805dfdd8c540e995c040a019828
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-04-19 13:10:36 +0200

    Update cats-effect to 2.1.3 (#1669)

--- project/Deps.scala
+++ project/Deps.scala
@@ -57,7 +57,7 @@ object Deps {
     // other settings.
 
     def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % versions.argonautShapeless)
-    def catsEffect = setting("org.typelevel" %%% "cats-effect" % "2.1.2")
+    def catsEffect = setting("org.typelevel" %%% "cats-effect" % "2.1.3")
     def fastParse = setting("com.lihaoyi" %%% "fastparse" % versions.fastParse)
     def scalaJsDom = setting("org.scala-js" %%% "scalajs-dom" % "1.0.0")
     def scalaJsJquery = setting("be.doeraene" %%% "scalajs-jquery" % "1.0.0")
