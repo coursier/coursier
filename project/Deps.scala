8310ebe91dbe1db3b8b7debd243a5b1269bfcd63
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-04-30 12:41:34 +0200

    Update data-class to 0.2.3 (#1687)

--- project/Deps.scala
+++ project/Deps.scala
@@ -19,7 +19,7 @@ object Deps {
   def argonautShapeless = "com.github.alexarchambault" %% "argonaut-shapeless_6.2" % versions.argonautShapeless
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M16"
   def catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
-  def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.2"
+  def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.3"
   def dockerClient = "com.spotify" % "docker-client" % "8.16.0"
   def emoji = "com.lightbend" %% "emoji" % "1.2.1"
   def fastParse = "com.lihaoyi" %% "fastparse" % versions.fastParse
