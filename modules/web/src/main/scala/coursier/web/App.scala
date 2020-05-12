047977006ff824b0dd74c003d358d4765c0551af
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-05-12 17:37:26 +0200

    Update default dependency in web app

--- modules/web/src/main/scala/coursier/web/App.scala
+++ modules/web/src/main/scala/coursier/web/App.scala
@@ -374,7 +374,7 @@ object App {
 
   val initialState = State(
     List(
-      Dependency(mod"io.get-coursier:coursier-cache_2.12", "1.1.0-M7") // DEBUG
+      Dependency(mod"io.get-coursier:coursier_2.13", "2.0.0-RC6-15") // DEBUG
     ),
     Seq("central" -> MavenRepository("https://repo1.maven.org/maven2")),
     ResolutionOptions(),
