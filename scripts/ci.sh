19ff0c8314c5afa9edca2835802f0995855ef431
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-09-22 11:56:31 +0200

    Refactor and rename CI script

---
+++ scripts/ci.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -evx
+
+if [ "$SCALA_JS" = 1 ]; then
+  sbt scalaFromEnv \
+    js/compile \
+    js/test:compile \
+    coreJS/fastOptJS \
+    cacheJS/fastOptJS \
+    testsJS/test:fastOptJS \
+    js/test:fastOptJS \
+    js/test
+elif [ "$NATIVE" = 1 ]; then
+  sbt scalaFromEnv \
+    launcher-native_03/publishLocal \
+    launcher-native_040M2/publishLocal \
+    cli/pack
+
+  modules/cli/target/pack/bin/coursier bootstrap \
+    -S \
+    -o native-echo \
+    io.get-coursier:echo_native0.3_2.11:1.0.1
+
+  if [ "$(./native-echo -n foo a)" != "foo a" ]; then
+    echo "Error: unexpected output from native test bootstrap." 1>&2
+    exit 1
+  fi
+else
+  sbt scalaFromEnv \
+    jvmProjects/compile \
+    jvmProjects/test:compile
+
+  if [ "$(uname)" == "Darwin" ]; then
+    IT="testsJVM/it:test" # doesn't run proxy-tests in particular
+  else
+    IT="jvmProjects/it:test"
+  fi
+
+  ./scripts/with-redirect-server.sh \
+    ./modules/tests/handmade-metadata/scripts/with-test-repo.sh \
+    sbt scalaFromEnv jvmProjects/test "$IT"
+
+  sbt scalaFromEnv evictionCheck compatibilityCheck
+fi
+
