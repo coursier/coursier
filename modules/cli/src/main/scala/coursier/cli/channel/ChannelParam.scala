a02349574ce577955376829c31750133e8ce0d3b
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-10-22 12:21:09 +0200

    fixup

--- modules/cli/src/main/scala/coursier/cli/channel/ChannelParam.scala
+++ modules/cli/src/main/scala/coursier/cli/channel/ChannelParam.scala
@@ -3,7 +3,6 @@ package coursier.cli.channel
 import cats.data.{NonEmptyList, Validated, ValidatedNel}
 import cats.implicits._
 import coursier.cli.params.OutputParams
-import coursier.install.Channel
 
 final case class ChannelParam(
   addChannel: List[String],
@@ -15,7 +14,7 @@ object ChannelParam {
   def apply(options: ChannelOptions, anyArg: Boolean): ValidatedNel[String, ChannelParam] = {
 
     val addChannelsV = options.add.traverse { s =>
-      val e = Channel
+      val e = coursier.install.Channel
         .parse(s)
         .left
         .map(NonEmptyList.one)
