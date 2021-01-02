77c3fdb72fd127884e07ce5b8146d4df7c2b9fba
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-01-02 15:58:10 +0100

    Explicitly use GraalVM to run native image scripts on Windows

--- .github/scripts/windows-build-native-image.bat
+++ .github/scripts/windows-build-native-image.bat
@@ -1,2 +1,2 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-bin\amm launcher.sc generateNativeImage --version 2.0-SNAPSHOT --output cs-test
+bin\cs launch ammonite:2.1.4-11-307f3d8 --scala 2.12.12 --jvm graalvm-ce-java11:20.1.0 -- launcher.sc generateNativeImage --version 2.0-SNAPSHOT --output cs-test
