44592b6546a88fce790c80fa0d03696e30bf3d05
Cyrille Chepelov <cyrille.chepelov@ebiznext.com>
2021-01-15 17:36:25 +0100

    chore: filter out Mima warnings (private class)

--- project/Mima.scala
+++ project/Mima.scala
@@ -100,6 +100,7 @@ object Mima {
         // private class
         (pb: Problem) => pb.matchName.forall(!_.startsWith("coursier.cache.CacheUrl#Args")),
         (pb: Problem) => pb.matchName.forall(!_.startsWith("coursier.cache.CacheUrl$Args")),
+        (pb: Problem) => pb.matchName.forall(!_.startsWith("coursier.cache.CacheUrl.BasicRealm")),
         // ignore shaded-stuff related errors
         (pb: Problem) => pb.matchName.forall(!_.startsWith("coursier.cache.shaded."))
       )
