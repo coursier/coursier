0332acec82fcaf87d7d74e36e6921b0a5eb9d92e
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-02-12 22:31:19 +0100

    Update cs-setup script

--- .github/scripts/cs-setup.sh
+++ .github/scripts/cs-setup.sh
@@ -8,8 +8,7 @@
 
 set -e
 
-coursier_version="2.0.0-RC6-24"
-for_graalvm="false"
+coursier_version="2.0.6"
 dest="cs"
 
 # Adapted from https://github.com/paulp/sbt-extras/blob/ea47026bd55439760f4c5e9b27b002fa64a8b82f/sbt#L427-L435 and around
@@ -32,7 +31,6 @@ process_args() {
     case "$1" in
       --version) require_arg version "$1" "$2" && coursier_version="$2" && shift 2 ;;
       --dest) require_arg path "$1" "$2" && dest="$2" && shift 2 ;;
-      --graalvm) for_graalvm="true" && shift ;;
       *) die "Unexpected argument: '$1'" ;;
     esac
   done
@@ -43,7 +41,6 @@ process_args "$@"
 
 do_chmod="1"
 ext=""
-setenv_bat="0"
 
 # https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux/17072017#17072017
 if [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "Linux" ]; then
@@ -58,11 +55,6 @@ else
   cache_base="$LOCALAPPDATA/Coursier/v1" # TODO Check that
   ext=".exe"
   do_chmod="0"
-
-  if [ "$for_graalvm" = "true" ]; then
-    choco install -y windows-sdk-7.1 vcbuildtools kb2519277
-    setenv_bat="1"
-  fi
 fi
 
 cache_dest="$cache_base/$(echo "$cs_url" | sed 's@://@/@')"
@@ -73,32 +65,13 @@ else
   mkdir -p "$(dirname "$cache_dest")"
   tmp_dest="$cache_dest.tmp-setup"
   echo "Downloading $cs_url"
-  curl -Lo "$tmp_dest" "$cs_url"
+  curl -fLo "$tmp_dest" "$cs_url"
   mv "$tmp_dest" "$cache_dest"
 fi
 
-if [ "$setenv_bat" = "1" ]; then
-  cp "$cache_dest" ".$dest$ext"
-  "./.$dest$ext" --help
-  if [ "$do_chmod" = "1" ]; then
-    chmod +x ".$dest$ext"
-  fi
-  cat > "$dest.bat" << EOF
-@call "C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Bin\\SetEnv.Cmd"
-%~dp0.$dest$ext %*
-EOF
-  # that one is for bash
-  cat > "$dest" << EOF
-#!/usr/bin/env bash
-set -e
-exec $(pwd)/$dest.bat "\$@"
-EOF
-  chmod +x "$dest" || true
-else
-  cp "$cache_dest" "$dest$ext"
-  if [ "$do_chmod" = "1" ]; then
-    chmod +x "$dest$ext"
-  fi
+cp "$cache_dest" "$dest$ext"
+if [ "$do_chmod" = "1" ]; then
+  chmod +x "$dest$ext"
 fi
 
 export PATH=".:$PATH"
