58186170e6ed161a1f1fcca1dbee87e4e42937e8
CALLE Ramiro <ramiro.sarmientocalle@mnt.fr>
2021-03-25 23:52:47 +0100

    Refactoring cli pattern matching coproduct options

--- modules/cli/src/main/scala/coursier/cli/CommandAppPreA.scala
+++ modules/cli/src/main/scala/coursier/cli/CommandAppPreA.scala
@@ -5,20 +5,20 @@ import caseapp.core.app.CommandAppWithPreCommand
 import caseapp.core.commandparser.CommandParser
 import caseapp.core.help.{CommandsHelp, Help}
 import caseapp.core.parser.Parser
-
+import shapeless.Coproduct
 // TODO Move to case-app
 
 /* The A suffix stands for anonymous */
-abstract class CommandAppPreA[D, T](
+abstract class CommandAppPreA[D, T <: Coproduct](
   beforeCommandParser: Parser[D],
   baseBeforeCommandMessages: Help[D],
   commandParser: CommandParser[T],
   commandsMessages: CommandsHelp[T]
 ) extends CommandAppWithPreCommand[D, T]()(beforeCommandParser, baseBeforeCommandMessages, commandParser, commandsMessages) {
 
-  def runA: RemainingArgs => T => Unit
+  def runA: T => RemainingArgs => Unit
 
   def run(options: T, remainingArgs: RemainingArgs): Unit =
-    runA(remainingArgs)(options)
+    runA(options)(remainingArgs)
 
 }
