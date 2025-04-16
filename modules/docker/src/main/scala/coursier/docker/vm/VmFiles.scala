25c5fe6e75afc7f538f98c45cd88005e1ebacd84
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2025-04-16 19:28:20 +0200

    Try to address disappearing bios package issues (#3380)

--- modules/docker/src/main/scala/coursier/docker/vm/VmFiles.scala
+++ modules/docker/src/main/scala/coursier/docker/vm/VmFiles.scala
@@ -6,6 +6,9 @@ import coursier.util.Artifact
 import coursier.cache.ArtifactError
 import java.io.File
 import coursier.cache.util.Cpu
+import scala.util.Using
+import java.math.BigInteger
+import java.security.MessageDigest
 
 final case class VmFiles(
   qemu: QemuFiles,
@@ -27,67 +30,85 @@ object VmFiles {
     }
 
   def default(
-    artifacts: QemuFiles.Artifacts = QemuFiles.Artifacts.default(),
+    artifactsTask: Task[QemuFiles.Artifacts] = QemuFiles.Artifacts.default(),
     imageArtifact: Artifact = defaultImageArtifact(),
     archiveCache: ArchiveCache[Task] = ArchiveCache()
-  ): Task[VmFiles] = {
-
+  ): Task[VmFiles] =
     // FIXME Quite some duplication with QemuFiles.default
+    artifactsTask.flatMap { artifacts =>
+      def baseArtifact(art: Artifact): Artifact =
+        if (art.url.contains("!")) art.withUrl(art.url.takeWhile(_ != '!'))
+        else art
 
-    def baseArtifact(art: Artifact): Artifact =
-      if (art.url.contains("!")) art.withUrl(art.url.takeWhile(_ != '!'))
-      else art
+      def taskFor(art: Artifact): Task[Either[ArtifactError, File]] =
+        if (art.url.contains("!")) archiveCache.get(art)
+        else archiveCache.cache.file(art).run
 
-    def taskFor(art: Artifact): Task[Either[ArtifactError, File]] =
-      if (art.url.contains("!")) archiveCache.get(art)
-      else archiveCache.cache.file(art).run
+      val artifacts0 = Seq(artifacts.qemu) ++
+        artifacts.bios.map(_._2).toSeq ++
+        artifacts.bios.map(_._1).toSeq ++
+        Seq(
+          artifacts.virtioRom,
+          imageArtifact
+        )
+      val downloadTask =
+        // First download every artifact with 'distinct', to work around "Attempts to download … twice" issues
+        // (coursier logger not accepting concurrent downloads of the same URL)
+        Task.gather.gather(artifacts0.map(baseArtifact).distinct.map(taskFor)).flatMap { _ =>
+          Task.gather.gather(artifacts0.map(taskFor))
+        }
 
-    val artifacts0 = Seq(artifacts.qemu) ++
-      artifacts.bios.toSeq ++
-      Seq(
-        artifacts.virtioRom,
-        imageArtifact
-      )
-    val downloadTask =
-      // First download every artifact with 'distinct', to work around "Attempts to download … twice" issues
-      // (coursier logger not accepting concurrent downloads of the same URL)
-      Task.gather.gather(artifacts0.map(baseArtifact).distinct.map(taskFor)).flatMap { _ =>
-        Task.gather.gather(artifacts0.map(taskFor))
-      }
-
-    downloadTask.flatMap { downloadResults =>
-      val errors = downloadResults.collect {
-        case Left(err) => err
-      }
-      errors match {
-        case Seq(first, others @ _*) =>
-          for (other <- others)
-            first.addSuppressed(other)
-          Task.fail(new Exception(first))
-        case Seq() =>
-          val (qemu, bios, virtioRom, image) =
-            if (artifacts.bios.isEmpty) {
-              val Seq(qemu0, virtioRom0, image0) = downloadResults.collect {
-                case Right(f) => os.Path(f)
-              }
-              (qemu0, None, virtioRom0, image0)
+      downloadTask.flatMap { downloadResults =>
+        val errors = downloadResults.collect {
+          case Left(err) => err
+        }
+        errors match {
+          case Seq(first, others @ _*) =>
+            for (other <- others)
+              first.addSuppressed(other)
+            Task.fail(new Exception(first))
+          case Seq() =>
+            val (qemu, bios, virtioRom, image) = artifacts.bios match {
+              case None =>
+                val Seq(qemu0, virtioRom0, image0) = downloadResults.collect {
+                  case Right(f) => os.Path(f)
+                }
+                (qemu0, None, virtioRom0, image0)
+              case Some((_, _, sha256)) =>
+                val Seq(qemu0, biosDepPkg, bios0, virtioRom0, image0) = downloadResults.collect {
+                  case Right(f) => os.Path(f)
+                }
+                // FIXME Ideally, we should check the sha-256 before letting ArchiveCache unpack things on disk
+                val rawDigest = Using.resource(os.read.inputStream(biosDepPkg)) { is =>
+                  val md   = MessageDigest.getInstance("SHA-256")
+                  val buf  = Array.ofDim[Byte](64 * 1024)
+                  var read = 0
+                  while ({
+                    read = is.read(buf)
+                    read >= 0
+                  })
+                    md.update(buf, 0, read)
+                  md.digest()
+                }
+                val rawChecksum = new BigInteger(1, rawDigest).toString(16)
+                val checksum    = "0" * (64 - rawChecksum.length) + rawChecksum
+                if (checksum == sha256)
+                  (qemu0, Some(bios0), virtioRom0, image0)
+                else
+                  throw new Exception(
+                    s"Invalid SHA-256 checksum for $bios0 (got $checksum, expected $sha256)"
+                  )
             }
-            else {
-              val Seq(qemu0, bios0, virtioRom0, image0) = downloadResults.collect {
-                case Right(f) => os.Path(f)
-              }
-              (qemu0, Some(bios0), virtioRom0, image0)
-            }
 
-          QemuFiles.postProcessQemuBinary(qemu)
+            QemuFiles.postProcessQemuBinary(qemu)
 
-          Task.point(
-            VmFiles(
-              QemuFiles(qemu, bios, virtioRom),
-              image
+            Task.point(
+              VmFiles(
+                QemuFiles(qemu, bios, virtioRom),
+                image
+              )
             )
-          )
+        }
       }
     }
-  }
 }
