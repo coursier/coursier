83a0bb7913c2f7476753013077db793657a9f3c9
Alex Archambault <alexandre.archambault@gmail.com>
2025-04-16 20:23:16 +0200

    Run setup in an isolated environment in tests

--- modules/cli-tests/src/main/scala/coursier/clitests/SetupTests.scala
+++ modules/cli-tests/src/main/scala/coursier/clitests/SetupTests.scala
@@ -2,11 +2,30 @@ package coursier.clitests
 
 import utest._
 
+import scala.util.Properties
+
 abstract class SetupTests extends TestSuite {
 
   def launcher: String
+  def assembly: String
+  def isNative: Boolean
+  def isNativeStatic: Boolean
 
-  val tests = Tests {
+  def hasDocker: Boolean =
+    Properties.isLinux
+
+  def alpineJavaImage =
+    sys.props.getOrElse(
+      "coursier.test.alpine-java-image",
+      sys.error("coursier.test.alpine-java-image Java property not set")
+    )
+  def alpineImage =
+    sys.props.getOrElse(
+      "coursier.test.alpine-image",
+      sys.error("coursier.test.alpine-image Java property not set")
+    )
+
+  def tests = Tests {
 
     test("setup") {
       TestUtil.withTempDir { tempDir =>
@@ -32,6 +51,50 @@ abstract class SetupTests extends TestSuite {
       }
     }
 
+    test("alpine-linux") {
+      if (hasDocker && !isNative) alpineLinuxTest(isNative = false)
+      else if (hasDocker && isNative && isNativeStatic) alpineLinuxTest(isNative = true)
+      else "Docker test disabled"
+    }
   }
+
+  def alpineLinuxTest(isNative: Boolean): Unit =
+    TestUtil.withTempDir { tmpDir0 =>
+      val tmpDir = os.Path(tmpDir0, os.pwd)
+      os.copy(
+        os.Path(if (isNative) launcher else assembly),
+        tmpDir / (if (isNative) "cs" else "cs.jar")
+      )
+
+      val args = Seq[os.Shellable](
+        "setup",
+        "--yes"
+      )
+
+      val baseCommand =
+        if (isNative)
+          Seq[os.Shellable](
+            "docker",
+            "run",
+            "--rm",
+            "-v",
+            s"$tmpDir:/shared",
+            alpineImage,
+            "/shared/cs"
+          )
+        else
+          Seq[os.Shellable](
+            "docker",
+            "run",
+            "--rm",
+            "-v",
+            s"$tmpDir:/shared",
+            alpineJavaImage,
+            "java",
+            "-jar",
+            "/shared/cs.jar"
+          )
+      os.proc(baseCommand, args).call(cwd = tmpDir, stdin = os.Inherit, stdout = os.Inherit)
+    }
 
 }
--- modules/cli-tests/src/test/scala/coursier/clitests/PackSetupTests.scala
+++ modules/cli-tests/src/test/scala/coursier/clitests/PackSetupTests.scala
@@ -1,5 +1,8 @@
 package coursier.clitests
 
 object PackSetupTests extends SetupTests {
-  val launcher = LauncherTestUtil.launcher
+  val launcher       = LauncherTestUtil.launcher
+  val assembly       = LauncherTestUtil.assembly
+  def isNative       = LauncherTestUtil.isNative
+  def isNativeStatic = LauncherTestUtil.isNativeStatic
 }
