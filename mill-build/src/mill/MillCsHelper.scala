487df583ff24137afe7d4d30037365104e911a4e
Alex Archambault <alexandre.archambault@gmail.com>
2025-07-24 15:21:12 +0200

    fixup

--- mill-build/src/mill/MillCsHelper.scala
+++ mill-build/src/mill/MillCsHelper.scala
@@ -4,6 +4,10 @@ import mill.*
 import mill.api.*
 
 object MillCsHelper {
-  def moduleCtxWithDiscover(ctx: ModuleCtx, discover: Discover, enclosingModule: ModuleCtx.Wrapper): ModuleCtx =
+  def moduleCtxWithDiscoverAndEnclosingModule(
+    ctx: ModuleCtx,
+    discover: Discover,
+    enclosingModule: ModuleCtx.Wrapper
+  ): ModuleCtx =
     ctx.withDiscover(discover).withEnclosingModule(enclosingModule)
 }
