d7d6b0b73b82943d132687e0fd64efc8b185b37c
Scala Steward <scala_steward@virtuslab.com>
2025-08-18 14:11:20 +0000

    Update utest to 0.9.1

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -70,7 +70,7 @@ object Deps {
     def svm                      = ivy"org.graalvm.nativeimage:svm:21.3.15"
     def tika                     = ivy"org.apache.tika:tika-core:3.2.2"
     def ujson                    = ivy"com.lihaoyi::ujson:4.2.1"
-    def utest                    = ivy"com.lihaoyi::utest::0.8.9"
+    def utest                    = ivy"com.lihaoyi::utest::0.9.1"
     def versions                 = ivy"io.get-coursier::versions::0.5.1"
     def windowsAnsi              = ivy"io.github.alexarchambault.windows-ansi:windows-ansi:0.0.6"
     def windowsAnsiPs =
