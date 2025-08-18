2c877de98234a818946be88c834193d9f327c72a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-08-18 23:57:55 +0200

    Update zstd-jni to 1.5.7-4 (#3444)

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -75,7 +75,7 @@ object Deps {
     def windowsAnsi              = ivy"io.github.alexarchambault.windows-ansi:windows-ansi:0.0.6"
     def windowsAnsiPs =
       ivy"io.github.alexarchambault.windows-ansi:windows-ansi-ps:${windowsAnsi.version}"
-    def zstdJni = ivy"com.github.luben:zstd-jni:1.5.7-3"
+    def zstdJni = ivy"com.github.luben:zstd-jni:1.5.7-4"
   }
 
   object Versions {
