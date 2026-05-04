363362ae3f7405bf744df2ec0dd8725458184e51
Jason Zaugg <jzaugg@gmail.com>
2026-05-05 08:07:18 +1000

    Update to versions 1.5.3
    This includes performance improvements.

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -70,7 +70,7 @@ object Deps {
     def tika                     = mvn"org.apache.tika:tika-core:3.2.3"
     def ujson                    = mvn"com.lihaoyi::ujson:4.3.2"
     def utest                    = mvn"com.lihaoyi::utest::0.9.1"
-    def versions                 = mvn"io.get-coursier::versions::0.5.1"
+    def versions                 = mvn"io.get-coursier::versions::0.5.3"
     def windowsAnsi              = mvn"io.github.alexarchambault.windows-ansi:windows-ansi:0.0.6"
     def windowsAnsiPs =
       mvn"io.github.alexarchambault.windows-ansi:windows-ansi-ps:${windowsAnsi.version}"
