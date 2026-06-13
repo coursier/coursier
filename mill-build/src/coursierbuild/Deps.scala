a8100daedf9bf543d3bf11abb5062700bee94480
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-13 15:05:58 +0200

    Update utest to 0.9.5 (#3691)

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -70,7 +70,7 @@ object Deps {
     def tika                     = mvn"org.apache.tika:tika-core:3.3.1"
     def undertow                 = mvn"io.undertow:undertow-core:2.3.18.Final"
     def ujson                    = mvn"com.lihaoyi::ujson:4.3.2"
-    def utest                    = mvn"com.lihaoyi::utest::0.9.1"
+    def utest                    = mvn"com.lihaoyi::utest::0.9.5"
     def versions                 = mvn"io.get-coursier::versions::0.5.3"
     def windowsAnsi              = mvn"io.github.alexarchambault.windows-ansi:windows-ansi:0.0.6"
     def windowsAnsiPs =
