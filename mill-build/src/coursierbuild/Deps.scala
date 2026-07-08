2b1483191686257d48b3fc8dcc5c90e5e17b1f0a
Scala Steward <scala_steward@virtuslab.com>
2026-07-08 15:41:52 +0000

    Update http4s-dsl, http4s-server to 0.23.36

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -81,7 +81,7 @@ object Deps {
 
   object Versions {
     def cats          = "2.13.0"
-    def http4s        = "0.23.34"
+    def http4s        = "0.23.36"
     def jniUtils      = "0.3.4"
     def jsoniterScala = "2.13.5"
     def scalaz        = "7.2.36"
