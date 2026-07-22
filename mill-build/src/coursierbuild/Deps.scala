23d5671f7818f235b50daf13a2b928cfd4c8fd5b
Scala Steward <scala_steward@virtuslab.com>
2026-07-22 15:59:54 +0000

    Update tika-core to 3.3.2

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -69,7 +69,7 @@ object Deps {
     def shapeless                = mvn"com.chuusai::shapeless:2.3.12"
     def slf4JNop                 = mvn"org.slf4j:slf4j-nop:2.0.18"
     def svm                      = mvn"org.graalvm.nativeimage:svm:21.3.18"
-    def tika                     = mvn"org.apache.tika:tika-core:3.3.1"
+    def tika                     = mvn"org.apache.tika:tika-core:3.3.2"
     def undertow                 = mvn"io.undertow:undertow-core:2.4.2.Final"
     def ujson                    = mvn"com.lihaoyi::ujson:4.3.2"
     def utest                    = mvn"com.lihaoyi::utest::0.9.5"
