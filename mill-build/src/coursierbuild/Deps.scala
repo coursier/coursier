05d6b2365de5195775e8944041092b5f88cd494e
Scala Steward <scala_steward@virtuslab.com>
2026-08-29 16:05:06 +0000

    Update zstd-jni to 1.5.7-16

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -77,7 +77,7 @@ object Deps {
     def windowsAnsi              = mvn"io.github.alexarchambault.windows-ansi:windows-ansi:0.0.6"
     def windowsAnsiPs =
       mvn"io.github.alexarchambault.windows-ansi:windows-ansi-ps:${windowsAnsi.version}"
-    def zstdJni = mvn"com.github.luben:zstd-jni:1.5.7-15"
+    def zstdJni = mvn"com.github.luben:zstd-jni:1.5.7-16"
   }
 
   object Versions {
