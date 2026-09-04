b19696703c814d58ae9975cbcf0b008c3c4467d9
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 16:57:28 +0000

    Update scala-library to 3.9.0

--- mill-build/src/coursierbuild/Deps.scala
+++ mill-build/src/coursierbuild/Deps.scala
@@ -111,7 +111,7 @@ object Deps {
 
   object ScalaVersions {
     def scala3   = "3.7.4"
-    def scala213 = "2.13.18"
+    def scala213 = "3.9.0"
     def scala212 = "2.12.20"
     // TODO SCALA_213_BASELINE search for this TODO in the codebase
     // for cleanup tasks when we move to Scala 2.13 as as the baseline
